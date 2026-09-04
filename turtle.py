dd255fb762fd79c3c097a812df6a9c27dba8768f
BatchBat <1293Umut+github@gmail.com>
2026-09-04 16:56:50 +0200

    uploaded the python file

---
+++ turtle.py
@@ -0,0 +1,10 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+t.pencolor("black")
+s.bgcolor("white")
+
+t.write("BatchBat", align="center", font=("Times New Roman", 40, "normal"))
+
+turtle.done()
